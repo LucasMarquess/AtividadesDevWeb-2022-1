8f0446dc68e812baa4e3f9783eddb3a05339dba6
Lucas Marques <lucas.fonseca@itixti.com.br>
2022-05-23 19:05:58 -0300

    inicio da lista 03

---
+++ Lista03/EX01.js
@@ -0,0 +1,8 @@
+$(document).ready( function(){
+    //EX 01 - A
+    let elements = $(".module");
+    console.log(elements)
+
+    //EX 01 - B
+    console.log($("#myList>li:eq(2)"), "terceiro elemento da lista");
+})
